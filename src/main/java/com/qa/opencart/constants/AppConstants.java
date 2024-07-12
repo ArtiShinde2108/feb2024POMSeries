e6d8151701485a4ade323a7cc7d127513c4d483d
Sarthak Shinde <sarthakshinde@Sarthaks-MacBook-Air.local>
2024-07-11 22:43:26 -0400

    added xmls

--- src/main/java/com/qa/opencart/constants/AppConstants.java
+++ src/main/java/com/qa/opencart/constants/AppConstants.java
@@ -7,7 +7,7 @@ public class AppConstants {
 	public static final String LOGIN_PAGE_TITLE = "Account Login";
 	public static final String ACCOUNTS_PAGE_TITLE = "My Account";
 	public static final String LOGIN_PAGE_FRACTION_URL = "route=account/login";
-	public static final String CONFIG_FILE_PATH = "./src/resources/config/config.properties";
+	public static final String CONFIG_FILE_PATH = "./src/test/resources/config/config.properties";
 	public static final String ACC_PAGE_FRACTION_VALUE = "route=account/account";
 	public static final List<String> ACC_PAGE_HEADERS_LIST = Arrays.asList("My Account", "My Orders",
 			"My Affiliate Account", "Newsletter");
